b491e9cee8acc13c4c7e1d192c9a33894616546c
nevengh <56957050+nevengh@users.noreply.github.com>
2025-04-02 14:01:11 +0300

    add arabic version to form

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -2,14 +2,70 @@ import "./About.css";
 import aboutImage from '../../assets/beautiful-young-girl-touching-her-face-youth-skin-care-concept(2).webp'
 import { useTranslation } from "react-i18next";
 import PageHero from "../../Components/PageHero/PageHero";
+import { motion } from "framer-motion";
+import { doctors } from "../../DB/Doctors";
+import { ArrowBTN, DoctorBox, Slider } from "../../Components";
+// Fade from bottom (like fadeInUp)
+const fadeInUp = {
+  initial: { opacity: 0, y: 50 },
+  whileInView: { opacity: 1, y: 0 },
+  transition: { duration: 1 },
+};
+
+// Fade from top (like fadeInDown)
+const fadeInDown = {
+  initial: { opacity: 0, y: -50 },
+  whileInView: { opacity: 1, y: 0 },
+  transition: { duration: 1 },
+};
 const About = () => {
-
-
-
 const {t} = useTranslation()
   return (
     <div>
       <PageHero BookUrl="/contact-us" Image={aboutImage} PageName={t('About')} pageSubhead={t('AboutUssubhead')}   />
+      <div className="aboutUs-Home">
+              <motion.p
+                className="about-title"
+                {...fadeInUp}
+                viewport={{ once: false }}
+              >
+                {t('WelcometoOurClinic')}
+                <br />
+                <span>The S</span>
+              </motion.p>
+      
+              <motion.p
+                className="about-text"
+                {...fadeInUp}
+                transition={{ duration: 1, delay: 0.2 }}
+                viewport={{ once: false }}
+              >
+                {t('WelcometoOurClinic_text')}
+              </motion.p>
+            </div>
+            <div className="PopularTreatment">
+        <motion.div
+          className="popularTreatment_Head"
+          {...fadeInDown}
+          viewport={{ once: false }}
+        >
+          <h1>Our Team</h1>
+          <ArrowBTN URL="" BTN_Name="View All Doctors" />
+        </motion.div>
+
+        <Slider
+          items={doctors}
+          renderItem={(doc) => (
+            <DoctorBox
+              key={doc.id}
+              DoctorName={doc.name}
+              DoctorSpec={doc.spec}
+              image={doc.img}
+              imgAlt={doc.name}
+            />
+          )}
+        />
+      </div>
     </div>
   );
 };
